89557a6d31547468c0cd3248dfe5acd61c0e9061
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-09 18:44:58 +0200

    Bloccato ciclo verifica numeri corretti

--- script.js
+++ script.js
@@ -3,3 +3,35 @@
 // se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+var number = [], indovinati = 0, numeriInseriti;
+
+function generated(max, min) {
+  for (var i = 0; i < 16; i++) {
+    var numeroGenerato = Math.floor(Math.random() * (max)) + min;
+    number.push(numeroGenerato);
+  }
+}
+
+generated(100, 1);
+
+console.log(number);
+
+  var h = 0;
+
+  for (var i = 0; i < 84; i++) {
+    h++;
+  numeriInseriti = parseInt(prompt("Inserisci un numero"));
+
+  for (var j = 0; j < number.length; j++) {
+
+    if (numeriInseriti == number[j]) {
+        console.log("Hai perso");
+        i = 84;
+    }
+  }
+  console.log("NUMERI INDOVINATI  " + h);
+
+
+
+  }
