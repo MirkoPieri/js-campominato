56fff6a89049fef1d52cfd942247d8f4e4dc7133
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-09 19:00:33 +0200

    Da rifare.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-var number = [], indovinati = 0, numeriInseriti;
+var number = [], indovinati = 0, numeriInseriti, elemento = document.getElementById('mio_id'), elemento1 = document.getElementById('mio_id1');
 
 function generated(max, min) {
   for (var i = 0; i < 16; i++) {
@@ -17,21 +17,16 @@ generated(100, 1);
 
 console.log(number);
 
-  var h = 0;
-
-  for (var i = 0; i < 84; i++) {
-    h++;
-  numeriInseriti = parseInt(prompt("Inserisci un numero"));
-
-  for (var j = 0; j < number.length; j++) {
-
-    if (numeriInseriti == number[j]) {
-        console.log("Hai perso");
-        i = 84;
+var j = 1;
+while (j <= 84) {
+    numeriInseriti = parseInt(prompt("Inserisci un numero"));
+    for (var i = 0; i < number.length; i++) {
+      if (number[i] == numeriInseriti) {
+        j = 85;
+        elemento.innerHTML = "Hai perso";
+        elemento1.innerHTML = indovinati;
+      }
     }
-  }
-  console.log("NUMERI INDOVINATI  " + h);
 
-
-
-  }
+    indovinati += 1;
+}
